895335bbfd0698479443afda8ee176b31c57c529
ianb <ianb@localhost>
2005-08-20 03:39:36 +0000

    Rearranged so the package is also a normal egg

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,13 +1,10 @@
 import os
 import sys
-import glob
 
 here = os.path.dirname(__file__)
 base = os.path.dirname(here)
 fake_packages = os.path.join(here, 'fake_packages')
-
-for egg_info_dir in glob.glob('%s/*/*.egg-info' % fake_packages):
-    sys.path.append(os.path.dirname(egg_info_dir))
+sys.path.append(fake_packages)
 
 # We can only import this after we adjust the paths
 import pkg_resources
