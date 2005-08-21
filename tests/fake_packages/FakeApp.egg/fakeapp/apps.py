2814ca0e2110b350bd0d687dff062eded51bfa8d
ianb <ianb@localhost>
2005-08-21 21:36:11 +0000

    New composit application

--- tests/fake_packages/FakeApp.egg/fakeapp/apps.py
+++ tests/fake_packages/FakeApp.egg/fakeapp/apps.py
@@ -13,3 +13,26 @@ def basic_app2(environ, start_response):
     
 def make_basic_app2(global_conf, **conf):
     return basic_app2
+
+def make_remote_addr(loader, global_conf, **conf):
+    apps = {}
+    addrs = {}
+    for name, value in conf.items():
+        if name.startswith('app.'):
+            apps[name[4:]] = loader.get_app(value, global_conf)
+        elif name.startswith('addr.'):
+            addrs[name[5:]] = value
+    dispatcher = RemoteAddrDispatch()
+    for name in apps:
+        dispatcher.map[addrs[name]] = apps[name]
+    return dispatcher
+
+class RemoteAddrDispatch(object):
+    def __init__(self, map=None):
+        self.map = map or {}
+
+    def __call__(self, environ, start_response):
+        addr = environ['REMOTE_ADDR']
+        app = self.map.get(addr) or self.map['0.0.0.0']
+        return app(environ, start_response)
+    
--- tests/fake_packages/FakeApp.egg/setup.py
+++ tests/fake_packages/FakeApp.egg/setup.py
@@ -9,5 +9,8 @@ setup(
       basic_app=fakeapp.apps:make_basic_app
       other=fakeapp.apps:make_basic_app2
       """,
+      'paste.composit_factory1': """
+      remote_addr=fakeapp.apps:make_remote_addr
+      """,
       },
     )
