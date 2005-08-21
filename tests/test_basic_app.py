a7d33ba0d32a01bd7201fca94f895fafb4d539fd
ianb <ianb@localhost>
2005-08-21 19:48:13 +0000

    First tests, first part of functionality

--- tests/test_basic_app.py
+++ tests/test_basic_app.py
@@ -1,12 +1,26 @@
-from paste.deploy import loadwsgi
+from paste.deploy import loadapp, loadfilter
 from fixture import *
+require('FakeApp')
 import fakeapp.apps
 
+here = os.path.dirname(__file__)
+
 def test_main():
-    app = loadtest('basic_app.ini')
+    app = loadapp('config:sample_configs/basic_app.ini',
+                  relative_to=here)
+    assert app is fakeapp.apps.basic_app
+    app = loadapp('config:sample_configs/basic_app.ini#main',
+                  relative_to=here)
+    assert app is fakeapp.apps.basic_app
+    app = loadapp('config:sample_configs/basic_app.ini',
+                  relative_to=here, name='main')
+    assert app is fakeapp.apps.basic_app
+    app = loadapp('config:sample_configs/basic_app.ini#ignored',
+                  relative_to=here, name='main')
     assert app is fakeapp.apps.basic_app
 
 def test_other():
-    app = loadtest('basic_app.ini', name='other')
+    app = loadapp('config:sample_configs/basic_app.ini#other',
+                  relative_to=here)
     assert app is fakeapp.apps.basic_app2
     
