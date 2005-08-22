d0bdcc4fe8d068721ea2f738f44f3cb65c456aaf
ianb <ianb@localhost>
2005-08-22 17:34:04 +0000

    Removed spurious modules which I had intended to port from previous experiment, but now I think I won't

--- paste/deploy/servers.py
+++
@@ -1,7 +0,0 @@
-def make_wsgiutils(host, port):
-    from wsgiutils import wsgiServer
-    def server(app):
-        server = wsgiServer.WSGIServer(
-            (host, int(port)), {'': app})
-        server.serve_forever()
-    return server
