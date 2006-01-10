203f51a6b0a165f9a75765a7db09dde722072ec0
ianb <ianb@localhost>
2006-01-10 08:39:52 +0000

    Improved the paster create --template=paste_deploy template, to include an entry point and sample application ready to run

--- paste/deploy/paster_templates.py
+++ paste/deploy/paster_templates.py
@@ -1,3 +1,4 @@
+import os
 from paste.script.templates import Template
 
 class PasteDeploy(Template):
@@ -8,3 +9,23 @@ class PasteDeploy(Template):
     egg_plugins = ['PasteDeploy']
     
     required_templates = ['PasteScript#basic_package']
+
+    def post(self, command, output_dir, vars):
+        for prereq in ['PasteDeploy']:
+            command.insert_into_file(
+                os.path.join(output_dir, 'setup.py'),
+                'Extra requirements',
+                '%r,\n' % prereq,
+                indent=True)
+        command.insert_into_file(
+            os.path.join(output_dir, 'setup.py'),
+            'Entry points',
+            ('      [paste.app_factory]\n'
+             '      main = %(package)s.wsgiapp:make_app\n') % vars,
+            indent=False)
+        if command.verbose:
+            print '*'*72
+            print '* Run "paster serve docs/devel_config.ini" to run the sample application'
+            print '* on http://localhost:8080'
+            print '*'*72
+        
