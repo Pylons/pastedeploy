3d5f36731626e323e8dbd05bd62510236f08bf92
ianb <ianb@localhost>
2006-08-10 17:41:14 +0000

    Added test for config middleware and exception

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,14 +3,12 @@ import sys
 
 here = os.path.dirname(__file__)
 base = os.path.dirname(here)
-fake_packages = os.path.join(here, 'fake_packages')
-sys.path.append(fake_packages)
+sys.path.insert(0, base)
 
 # We can only import this after we adjust the paths
 import pkg_resources
 
 # Make absolutely sure we're testing *this* package, not
 # some other installed package
-sys.path.insert(0, base)
-#pkg_resources.require('Paste-Deploy')
+pkg_resources.require('PasteDeploy')
 
