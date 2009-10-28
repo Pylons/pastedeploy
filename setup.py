f861ae88b112e457983f36608d7b41fd3161be4d
Ian Bicking <ianb@colorstudy.com>
2009-10-28 01:41:35 -0500

    moved repo

--- setup.py
+++ setup.py
@@ -12,8 +12,9 @@ URIs; these URIs can refer to Python Eggs for INI-style configuration
 files.  `Paste Script <http://pythonpaste.org/script>`_ provides
 commands to serve applications based on this configuration file.
 
-The latest version is available in a `Subversion repository
-<http://svn.pythonpaste.org/Paste/Deploy/trunk#egg=PasteDeploy-dev>`_.
+The latest version is available in a `Mercurial repository
+<http://bitbucket.org/ianb/pastedeploy>`_ (or a `tarball
+<http://bitbucket.org/ianb/pastedeploy/get/tip.gz#egg=PasteDeploy-dev>`_).
 
 For the latest changes see the `news file
 <http://pythonpaste.org/deploy/news.html>`_.
