113f5613942fbfd201c53781a2f6eb7a028d5c97
Alex Gr?nholm <alex.gronholm@nextday.fi>
2011-05-17 03:17:44 +0300

    PEP 8 cleanups

--- paste/deploy/epdesc.py
+++ paste/deploy/epdesc.py
@@ -3,6 +3,7 @@ class AppFactoryDescription(object):
     This gives a factory/function that can create WSGI apps
     """
 
+
 class CompositeFactoryDescription(object):
     description = """
     This gives a factory/function that can create WSGI apps, and has
@@ -10,11 +11,13 @@ class CompositeFactoryDescription(object):
     apps based on name.
     """
 
+
 class FilterAppFactoryDescription(object):
     description = """
     This gives a factory/function that wraps a WSGI application to
     create another WSGI application (typically applying middleware)
     """
+
 
 class FilterFactoryDescription(object):
     description = """
@@ -23,12 +26,14 @@ class FilterFactoryDescription(object):
     paste.filter_app_factory is the same thing with less layers.
     """
 
+
 class ServerFactoryDescription(object):
     description = """
     This gives a factory/function that creates a server, that can be
     called with a WSGI application to run indefinitely.
     paste.server_runner is the same thing with less layers.
     """
+
 
 class ServerRunnerDescription(object):
     description = """
