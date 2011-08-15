9e8069125c369594040b55f6152fb4fab23b9c9b
Alex Gr?nholm <alex.gronholm@nextday.fi>
2011-08-15 15:06:50 +0300

    Moved the description to README, switched version to development

--- setup.py
+++ setup.py
@@ -2,46 +2,34 @@ from setuptools import setup, find_packages
 
 
 setup(
-    name="PasteDeploy",
-    version='1.5.0',
-    description="Load, configure, and compose WSGI applications and servers",
-    long_description="""\
-This tool provides code to load WSGI applications and servers from
-URIs; these URIs can refer to Python Eggs for INI-style configuration
-files.  `Paste Script <http://pythonpaste.org/script>`_ provides
-commands to serve applications based on this configuration file.
-
-The latest version is available in a `Mercurial repository
-<http://bitbucket.org/ianb/pastedeploy>`_ (or a `tarball
-<http://bitbucket.org/ianb/pastedeploy/get/tip.gz#egg=PasteDeploy-dev>`_).
-
-For the latest changes see the `news file
-<http://pythonpaste.org/deploy/news.html>`_.
-""",
+    name='PasteDeploy',
+    version='1.5.1.dev1',
+    description='Load, configure, and compose WSGI applications and servers',
+    long_description=open('README').read(),
     classifiers=[
-      "Development Status :: 5 - Production/Stable",
-      "Intended Audience :: Developers",
-      "License :: OSI Approved :: MIT License",
-      "Programming Language :: Python",
-      "Programming Language :: Python :: 2.5",
-      "Programming Language :: Python :: 2.6",
-      "Programming Language :: Python :: 2.7",
-      "Programming Language :: Python :: 3",
-      "Programming Language :: Python :: 3.1",
-      "Programming Language :: Python :: 3.2",
-      "Topic :: Internet :: WWW/HTTP",
-      "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
-      "Topic :: Internet :: WWW/HTTP :: WSGI",
-      "Topic :: Internet :: WWW/HTTP :: WSGI :: Middleware",
-      "Topic :: Software Development :: Libraries :: Python Modules",
-      "Framework :: Paste",
+      'Development Status :: 5 - Production/Stable',
+      'Intended Audience :: Developers',
+      'License :: OSI Approved :: MIT License',
+      'Programming Language :: Python',
+      'Programming Language :: Python :: 2.5',
+      'Programming Language :: Python :: 2.6',
+      'Programming Language :: Python :: 2.7',
+      'Programming Language :: Python :: 3',
+      'Programming Language :: Python :: 3.1',
+      'Programming Language :: Python :: 3.2',
+      'Topic :: Internet :: WWW/HTTP',
+      'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+      'Topic :: Internet :: WWW/HTTP :: WSGI',
+      'Topic :: Internet :: WWW/HTTP :: WSGI :: Middleware',
+      'Topic :: Software Development :: Libraries :: Python Modules',
+      'Framework :: Paste',
       ],
     keywords='web wsgi application server',
-    author="Ian Bicking",
-    author_email="ianb@colorstudy.com",
-    maintainer="Alex Gronholm",
-    maintainer_email="alex.gronholm@nextday.fi",
-    url="http://pythonpaste.org/deploy/",
+    author='Ian Bicking',
+    author_email='ianb@colorstudy.com',
+    maintainer='Alex Gronholm',
+    maintainer_email='alex.gronholm@nextday.fi',
+    url='http://pythonpaste.org/deploy/',
     license='MIT',
     namespace_packages=['paste'],
     packages=find_packages(exclude=['tests']),
