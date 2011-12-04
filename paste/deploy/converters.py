5adb7d67167fd476d3d1769224c288b7789a32f5
Alexandre Conrad <alexandre.conrad@gmail.com>
2011-12-03 18:40:08 -0800

    use frozenset for asbool() lookup

--- paste/deploy/converters.py
+++ paste/deploy/converters.py
@@ -3,12 +3,16 @@
 from paste.deploy.compat import basestring
 
 
+truthy = frozenset(['true', 'yes', 'on', 'y', 't', '1'])
+falsy = frozenset(['false', 'no', 'off', 'n', 'f', '0'])
+
+
 def asbool(obj):
     if isinstance(obj, basestring):
         obj = obj.strip().lower()
-        if obj in ['true', 'yes', 'on', 'y', 't', '1']:
+        if obj in truthy:
             return True
-        elif obj in ['false', 'no', 'off', 'n', 'f', '0']:
+        elif obj in falsy:
             return False
         else:
             raise ValueError("String is not true/false: %r" % obj)
