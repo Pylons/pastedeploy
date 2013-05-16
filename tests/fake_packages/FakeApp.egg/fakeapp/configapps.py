bf663458722b48b73178356bd7e28dfc0456c75f
Tres Seaver <tseaver@palladion.com>
2013-05-16 13:35:18 -0400

    Py3k-compatible sorting of 'kwargs.items'.

---
+++ tests/fake_packages/FakeApp.egg/fakeapp/configapps.py
@@ -0,0 +1,14 @@
+class SimpleApp(object):
+    def __init__(self, global_conf, local_conf, name):
+        self.global_conf = global_conf
+        self.local_conf = local_conf
+        self.name = name
+
+    def __call__(self, environ, start_response):
+        start_response('200 OK', [('Content-type', 'text/html')])
+        return ['I am: ', name]
+        
+    def make_app(cls, global_conf, **conf):
+        return cls(global_conf, conf, 'basic')
+    make_app = classmethod(make_app)
+
---
+++ tests/test_converters.py
@@ -0,0 +1,17 @@
+def test_asbool_truthy():
+    from paste.deploy.converters import asbool
+    assert asbool('true')
+    assert asbool('yes')
+    assert asbool('on')
+    assert asbool('y')
+    assert asbool('t')
+    assert asbool('1')
+
+def test_asbool_falsy():
+    from paste.deploy.converters import asbool
+    assert not asbool('false')
+    assert not asbool('no')
+    assert not asbool('off')
+    assert not asbool('n')
+    assert not asbool('f')
+    assert not asbool('0')
