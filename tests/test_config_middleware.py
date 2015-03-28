cf7e560f89016a302342821f5071941a09c04283
hao huang <huang1hao@gmail.com>
2015-03-28 08:10:39 +0000

    Fix typo in docs/index.txt

---
+++ tests/test_config_middleware.py
@@ -0,0 +1,28 @@
+from nose.tools import assert_raises
+from nose.plugins.skip import SkipTest
+
+from paste.deploy.config import ConfigMiddleware
+
+
+class Bug(Exception):
+    pass
+
+
+def app_with_exception(environ, start_response):
+    def cont():
+        yield "something"
+        raise Bug
+    start_response('200 OK', [('Content-type', 'text/html')])
+    return cont()
+
+
+def test_error():
+    # This import is conditional due to Paste not yet working on py3k
+    try:
+        from paste.fixture import TestApp
+    except ImportError:
+        raise SkipTest
+
+    wrapped = ConfigMiddleware(app_with_exception, {'test': 1})
+    test_app = TestApp(wrapped)
+    assert_raises(Bug, test_app.get, '/')
