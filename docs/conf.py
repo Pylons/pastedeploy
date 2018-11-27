00b791de28f6862a8b533d5787ca622b3123a9f6
Steve Piercy <web@stevepiercy.com>
2018-11-27 04:58:47 -0800

    Add intersphinx and configure Python docs

--- docs/conf.py
+++ docs/conf.py
@@ -23,7 +23,15 @@ import pylons_sphinx_themes
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.autodoc']
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.intersphinx',
+]
+
+# Looks for objects in external projects
+intersphinx_mapping = {
+    'python': ('https://docs.python.org/3/', None),
+}
 
 # Add any paths that contain templates here, relative to this directory.
 # templates_path = ['_templates']
