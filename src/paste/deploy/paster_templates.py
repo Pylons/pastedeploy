20cf43a72d19a35834658f333b126cd5f1a0df15
Francis Charette Migneault <francis.charette.migneault@gmail.com>
2023-11-06 13:31:43 -0500

    apply format fixes

--- src/paste/deploy/paster_templates.py
+++ src/paste/deploy/paster_templates.py
@@ -6,7 +6,6 @@ from paste.script.templates import Template
 
 
 class PasteDeploy(Template):
-
     _template_dir = 'paster_templates/paste_deploy'
     summary = "A web application deployed through paste.deploy"
 
